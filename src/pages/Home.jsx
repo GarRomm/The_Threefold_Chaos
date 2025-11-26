373b418668a75e4c4692a5ad56a398ee3e0eedbb
Romain garcia <romain.garcia@santor.net>
2025-11-26 11:55:14 +0100

    Replace logo with PNG version and add character decorations on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.png'
 import frame from '../assets/frame.svg'
+import character1 from '../assets/character1.svg'
+import character2 from '../assets/character2.svg'
 import './Home.css'
 
 function Home({ onStart }) {
   return (
     <div className="page home-page">
       <img src={frame} alt="" className="page-frame" />
+      <img src={character1} alt="" className="character character-left" />
+      <img src={character2} alt="" className="character character-right" />
       <div className="home-content">
         <img src={logo} alt="The Threefold Chaos Logo" className="logo" />
         <h1 className="title">The Threefold Chaos</h1>
